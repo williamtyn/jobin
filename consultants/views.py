a67bae7eb9f0e1a54fc20e53aa9efad72e51714e
williamtyn <william@tyner.se>
2022-11-14 20:03:00 +0000

    add orderlist.html and view for order

--- consultants/views.py
+++ consultants/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Order
 
-# Create your views here.
+
+class OrderList(generic.ListView):
+    model = Order
+    queryset = Order.objects.filter(status=0).order_by('created_date')
+    template_name = 'orderlist.html'
+
+
