d7192a130d31e7f63eca1ccc405b440b1d4af0c0
williamtyn <william@tyner.se>
2022-11-15 13:31:55 +0000

    installed allauth and add allauth templates

--- consultants/admin.py
+++ consultants/admin.py
@@ -1,7 +1,16 @@
 from django.contrib import admin
-from .models import Order
+from django.contrib.auth.admin import UserAdmin
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+from .models import Order, User
 
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
     list_filter = ('status', 'created_date')
+
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = User
+    list_display = ["email", "username", ]
--- jobin/urls.py
+++ jobin/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('consultants.urls'), name='consultants_urls'),
+    path('accounts/', include('allauth.urls')),
 ]
