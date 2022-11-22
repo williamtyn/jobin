8c97a7eb5cf655c4700d9054948dffff33fe4eb7
williamtyn <william@tyner.se>
2022-11-22 15:20:11 +0000

    change signup form to save user type

--- consultants/forms.py
+++ consultants/forms.py
@@ -4,21 +4,24 @@ from django.contrib.auth.forms import UserCreationForm
 from .models import User, Order
 
 
+# Signup form for user to be flagged as customer or partner
 class SignUpForm(UserCreationForm):
     company = forms.CharField(
-        label='Company Inc', max_length=50, min_length=2)
-    vat_no = forms.IntegerField()
+        label='Company Name', max_length=50, min_length=2)
+    vat_no = forms.CharField(max_length=50, label='Enter your VAT')
+    CHOICES = ((0, 'Customer'), (1, 'Partner'))
+    user_type = forms.ChoiceField(widget=forms.Select, choices=CHOICES)
 
-    class Meta:
+    class Meta(UserCreationForm.Meta):
         model = User
         fields = ('username',
-                  'first_name',
-                  'last_name',
                   'email',
                   'company',
-                  'vat_no', )
+                  'vat_no',
+                  'user_type',)
 
 
+# Form for user to create new order
 class NewOrderForm(forms.ModelForm):
     class Meta:
         model = Order
