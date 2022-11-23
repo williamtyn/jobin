d544d9c272aae860dae97fa49981f8655fb46454
williamtyn <william@tyner.se>
2022-11-23 12:56:06 +0000

    add candidate to adminpanel

--- consultants/admin.py
+++ consultants/admin.py
@@ -1,12 +1,17 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .forms import UserCreationForm
-from .models import Order, User
+from .models import Order, User, Candidate
 
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
     list_filter = ('status', 'created_date')
+
+
+@admin.register(Candidate)
+class CandidateAdmin(admin.ModelAdmin):
+    list_filter = ('presented_date',)
 
 
 fields = list(UserAdmin.fieldsets)
