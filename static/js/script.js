286203d4e3be122ba8aa3fe3fac06cbf7315fb34
williamtyn <william@tyner.se>
2022-11-27 21:19:11 +0000

    add back button and toggle footer in javascript

--- static/js/script.js
+++ static/js/script.js
@@ -9,5 +9,34 @@ setTimeout(function() {
 const toggleCandidates = (e) => {
     const sibling = e.target.closest('div');
     sibling.nextElementSibling.classList.toggle('toggle');
-    }
+};
 
+// Show footer when user are at bottom of page
+const scrollFooter = document.getElementById('showfooter')
+const handleScroll = () => {
+    if (window.innerHeight + window.pageYOffset >= document.body.offsetHeight) {
+       scrollFooter.style.display = 'block';
+    } else {
+        scrollFooter.style.display = 'none';
+    }
+}
+    
+window.addEventListener('scroll', handleScroll)
+
+// Back to top with button
+// Button that takes user back to top
+const scrollBtn = document.getElementById('scrollbtn')
+
+// When the user scrolls down 200px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+    scrollBtn.style.display = 'block';
+  } else {
+    scrollBtn.style.display = 'none';
+  };
+};
+
+// When the user clicks on the button, scroll to the top of the document
+const toTheTop = () => window.scrollTo({top: 0, behavior:'smooth'});
